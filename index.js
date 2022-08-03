eb55a70aaaf157a24b3636e795ed58c0bd787e53
Milan Contreras <milan.contreras@epn.edu.ec>
2022-08-03 16:46:04 -0500

    port in case the api is deployed

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.use((req, res) => {
   res.status(404).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => { // async function
   console.log(`Server is running on port ${PORT}`)
 })
